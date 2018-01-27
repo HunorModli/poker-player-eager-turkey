d3f5b09752068c10f1a2aba0fd02c9d27728fea5
Módli Hunor <mhuni95@gmail.com>
2018-01-27 12:04:13 +0100

    default pairs to fold

--- player.php
+++ player.php
@@ -6,18 +6,32 @@ class Player
 
     public function betRequest($game_state)
     {
-//        $game_state
+        $filePairs = file_get_contents('../fold.txt');
 
-        $string = file_get_contents("player-api.json");
-        $json_a = json_decode($string, true);
+        $pairsToFold = explode('|', $filePairs);
 
-        $rnd = rand(0,100);
+        $round = $game_state['round'];
 
-        if ($rnd < 30) {
-            return 0;
-        } else {
-            return 1000000;
+        $self = null;
+
+        foreach ($game_state['players'] as $player) {
+            if (array_key_exists('hole_cards', $player)) {
+                $self = $player;
+            }
         }
+
+        $card1 = new Card($self['hole_cards']['0']['rank'],$self['hole_cards']['0']['suite']);
+        $card2 = new Card($self['hole_cards']['1']['rank'],$self['hole_cards']['1']['suite']);
+
+        foreach ($pairsToFold as $item) {
+            $tmp = explode('-',$item);
+            if (($card1->getRank() == $tmp[0] && $card2->getRank() == $tmp[1]) ||
+                ($card1->getRank() == $tmp[1] && $card2->getRank() == $tmp[0])) {
+                return 0;
+            }
+        }
+
+        return 100000;
     }
 
     public function showdown($game_state)
