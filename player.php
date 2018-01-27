d2707da3b2cd3086e13c87cf1cca2a1da72d51d8
Módli Hunor <mhuni95@gmail.com>
2018-01-27 14:10:37 +0100

    back to allin machine

--- player.php
+++ player.php
@@ -12,7 +12,17 @@ class Card
 
     public function __construct($rank, $suit)
     {
-        $this->rank = $rank;
+        if (intval($rank) <= 10) {
+            $this->rank = intval($rank);
+        } else if ($rank = 'J') {
+            $this->rank = 11;
+        } else if ($rank = 'Q') {
+            $this->rank = 12;
+        } else if ($rank = 'K') {
+            $this->rank = 13;
+        } else if ($rank = 'A') {
+            $this->rank = 14;
+        }
         $this->suit = $suit;
     }
 
@@ -55,52 +65,59 @@ class Player
 
     public function betRequest($game_state)
     {
-        try {
-            $filePairs = file_get_contents('fold.txt');
-
-            $pairsToFold = explode('|', $filePairs);
-
-            $round = $game_state['round'];
-
-            $self = null;
-
-            foreach ($game_state['players'] as $player) {
-                if (array_key_exists('hole_cards', $player)) {
-                    $self = $player;
-                }
-//                else {
-//                    if ($player['status'] == 'active') {
+//        try {
+//            $filePairs = file_get_contents('fold.txt');
 //
-//                    }
+//            $pairsToFold = explode('|', $filePairs);
+//
+//            $round = $game_state['round'];
+//
+//            $self = null;
+//
+//            foreach ($game_state['players'] as $player) {
+//                if (array_key_exists('hole_cards', $player)) {
+//                    $self = $player;
 //                }
-            }
+////                else {
+////                    if ($player['status'] == 'active') {
+////
+////                    }
+////                }
+//            }
+//
+//            $card1 = new Card($self['hole_cards']['0']['rank'],$self['hole_cards']['0']['suit']);
+//            $card2 = new Card($self['hole_cards']['1']['rank'],$self['hole_cards']['1']['suit']);
+//
+//            foreach ($pairsToFold as $item) {
+//                $tmp = explode('-',$item);
+//                if (($card1->getRank() == $tmp[0] && $card2->getRank() == $tmp[1]) ||
+//                    ($card1->getRank() == $tmp[1] && $card2->getRank() == $tmp[0])) {
+//                    $this->log("FOLD BY HAND");
+//                    return 0;
+//                }
+//            }
+//
+//            if ($game_state['current_buy_in'] >= 300 && $card1->getRank() != $card2->getRank()) {
+//                $this->log("BUY IN > 300");
+//                return 0;
+//            }
+//
+//            if ($game_state['pot'] < 100) {
+//                $this->log("POT < 100");
+//                return 100;
+//            }
+//            $this->log("Return 10000");
+//            return 100000;
+//        } catch (\Exception $e) {
+//            $this->log("EXCEPTION: " . $e->getMessage());
+//            return 100000;
+//        }
 
-            $card1 = new Card($self['hole_cards']['0']['rank'],$self['hole_cards']['0']['suit']);
-            $card2 = new Card($self['hole_cards']['1']['rank'],$self['hole_cards']['1']['suit']);
-
-            foreach ($pairsToFold as $item) {
-                $tmp = explode('-',$item);
-                if (($card1->getRank() == $tmp[0] && $card2->getRank() == $tmp[1]) ||
-                    ($card1->getRank() == $tmp[1] && $card2->getRank() == $tmp[0])) {
-                    $this->log("FOLD BY HAND");
-                    return 0;
-                }
-            }
-
-            if ($game_state['current_buy_in'] >= 300 && $card1->getRank() != $card2->getRank()) {
-                $this->log("BUY IN > 300");
-                return 0;
-            }
-
-            if ($game_state['pot'] < 100) {
-                $this->log("POT < 100");
-                return 100;
-            }
-            $this->log("Return 10000");
-            return 100000;  
-        } catch (\Exception $e) {
-            $this->log("EXCEPTION: " . $e->getMessage());
-            return 100000;
+        $rnd = rand(0,100);
+        if ($rnd < 30) {
+            return 0;
+        } else {
+            return 10000;
         }
     }
 
